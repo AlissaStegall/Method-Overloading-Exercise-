33a6c9f397f69ec2212e46ba6b3516d56c795906
AlissaStegall <alissaistegall@gmail.com>
2023-04-26 12:43:55 -0700

    created method overloads

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,36 @@
 {
     public class Program
     {
+            public static int Add(int x, int y)
+            {
+                return x + y;
+            }
+            public static decimal Add(decimal c, decimal b)
+            {
+                return c + b;
+            }
+            public static string Add(int x, int y, bool isTrue)
+            {
+            var sum = 0;
+            var response = "";
+                if (isTrue)
+                {
+                sum = x + y;
+
+                    if (sum == 1)
+                    {
+                    response = $"{sum} dollar.";
+                    }
+                    else
+                    {
+                    response = $"{sum} dollars.";
+                    }
+                }
+            return response;
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
